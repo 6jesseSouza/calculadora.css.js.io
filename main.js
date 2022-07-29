4e61060b8e2c8bd54e1e21f6902e47474beeb18e
Jessé <91789009+6jesseSouza@users.noreply.github.com>
2022-07-29 13:30:00 -0300

    Update main.js

--- main.js
+++ main.js
@@ -28,59 +28,6 @@ if(resultado)
  }
 
 
-/*
-
-//aqui é uma lista d Array
-const produtos = [
-   { id:1, nome:'detergente', valor:2.00 , categoria: 'limpeza' },
-   { id:2, nome:'amaciante', valor: 6.50, categoria: 'limpeza'},
-   { id:3, nome:'pao', valor:2.00 , categoria: 'alimento' },
-   { id:4, nome:'queijo', valor:7.00 , categoria: 'alimento' },
-   { id:5, nome:'leite', valor:2.20 , categoria: 'alimento' },
-];
-
-const ids= produtos.map(d=>d.nome,
-    'alimento');
-console.log(ids);
-
-
-
-
-
-
-const s ={
-    user: {
-         id:1,
- fristName:'lo',
- lastName:'da silva'},
-
-    i: [
-        {
-            p: {id: 1, n:'maçã'},
-            q: 10,
-        },
-        {
-            p: {id: 2, n:'laranja'},
-            q: 1,
-        },
-    ]
-}
-
-
-function formatacao(s){
-s.user.name =
-`{s.user.fristName }${s} `
-delete s.user.fristName;
-delete s.user.lastName;
-
-}
-
-console.log(s.i);
-formatacao(s);
-*/
-
-
-
 
 
 
